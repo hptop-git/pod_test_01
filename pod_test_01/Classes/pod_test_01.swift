b5127ecbdee4b5834f55d3a43eb385766727422a
hptop <hptop@knou.ac.kr>
2022-10-04 09:43:27 +0900

    file modify

--- pod_test_01/Classes/pod_test_01.swift
+++ pod_test_01/Classes/pod_test_01.swift
@@ -5,4 +5,8 @@ public struct Hello {
     public static func sayHello(with name: String) {
         print("\(name) 안녕!!")
     }
+    
+    public static func sayBye(with name: String) {
+        print("\(name) 잘가!!")
+    }
 }
