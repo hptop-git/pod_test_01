9d641a9447a821b4305720840e05cd6bc0ea64fa
hptop <hptop@knou.ac.kr>
2022-10-04 09:18:14 +0900

    second commit

---
+++ Example/Pods/Target Support Files/Pods-pod_test_01_Example/Pods-pod_test_01_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pod_test_01_Example : NSObject
+@end
+@implementation PodsDummy_Pods_pod_test_01_Example
+@end
---
+++ Example/Pods/Target Support Files/pod_test_01/pod_test_01-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_pod_test_01 : NSObject
+@end
+@implementation PodsDummy_pod_test_01
+@end
